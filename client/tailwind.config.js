d0d96861cdb03d126a2486317e5428e0d6d9f26a
joelmajn <144187159+joelmajn@users.noreply.github.com>
2025-07-31 18:28:36 -0300

    ad

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,7 @@ const pluginTypography = require("@tailwindcss/typography");
 
 module.exports = {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
